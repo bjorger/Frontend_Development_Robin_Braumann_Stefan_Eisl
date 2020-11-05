36900d1a374df894f21292c9d2eb82db524cfbdd
Robin <robin.braumann@gmail.com>
2020-11-05 17:33:45 +0100

    Fix issue with deleting properties from original

--- question.js
+++ question.js
@@ -1,37 +1,38 @@
-const question1 = {
-    question: 'Whats the best video game ever made?',
-    correctAnswer: 'a',
-    a: 'World of Warcraft Pre BFA',
-    b: 'World of Warcraft Battle of Azeroth',
-    c: 'Binding of Isaac',
-    d: 'Super Mario Maker 2'
-}
-
-const question2 = {
-    question: 'Whats 2 + 2?',
-    correctAnswer: 'c',
-    a: '5',
-    b: '7',
-    c: '4',
-    d: '8'
-}
-
-const question3 = {
-    question: 'Whats the capital of austria?',
-    correctAnswer: 'b',
-    a: 'Paris',
-    b: 'Vienna',
-    c: 'Halmstad',
-    d: 'Salzburg'
-}
-
-const question4 = {
-    question: 'Whats the plural of regex?',
-    correctAnswer: 'c',
-    a: 'Regulars Expression',
-    b: 'Regexes',
-    c: 'Regular Expressions',
-    d: 'Regrets'
-}
-
-export const questions = [question1, question2, question3, question4]
+export const questions = [
+	{
+		id: 0,
+		question: 'Whats the best video game ever made?',
+		correctAnswer: 'a',
+		a: 'World of Warcraft Pre BFA',
+		b: 'World of Warcraft Battle of Azeroth',
+		c: 'Binding of Isaac',
+		d: 'Super Mario Maker 2',
+	},
+	{
+		id: 1,
+		question: 'Whats 2 + 2?',
+		correctAnswer: 'c',
+		a: '5',
+		b: '7',
+		c: '4',
+		d: '8',
+	},
+	{
+		id: 2,
+		question: 'Whats the capital of austria?',
+		correctAnswer: 'b',
+		a: 'Paris',
+		b: 'Vienna',
+		c: 'Halmstad',
+		d: 'Salzburg',
+	},
+	{
+		id: 3,
+		question: 'Whats the plural of regex?',
+		correctAnswer: 'c',
+		a: 'Regulars Expression',
+		b: 'Regexes',
+		c: 'Regular Expressions',
+		d: 'Regrets',
+	},
+];
--- quiz.js
+++ quiz.js
@@ -1,17 +1,31 @@
-import {questions} from './question.js'
+import { questions } from './question.js';
 
 // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/math.random
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-export function askQuestion(){
-    let question = questions[getRandomInt(questions.length)]
-    delete question.correctAnswer
-    return question
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
-export function answerQuestion(user_question, answer){
+export function askQuestion() {
+    // Make deep copy of questions, so you can manipulate the properties
+    const questions_copy = JSON.parse(JSON.stringify(questions))
+    var question = questions_copy[getRandomInt(questions.length)];
+    delete question.correctAnswer
+	return question;
+}
+
+export function answerQuestion(user_question, answer) {
+    console.log(answer)
+    console.log(questions[user_question.id].correctAnswer)
+    console.log(questions[user_question.id])
+	if (answer === questions[user_question.id].correctAnswer) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/** Solution without a Question ID
+ export function answerQuestion(user_question, answer){
     questions.forEach(question => {
         if (question.question === user_question){
             if(answer === question.correctAnswer){
@@ -23,3 +37,5 @@ export function answerQuestion(user_question, answer){
         } 
     })
 }
+ * 
+ */
