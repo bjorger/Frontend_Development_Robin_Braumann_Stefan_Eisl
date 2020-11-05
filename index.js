e47d50f9446fc66c52e0cf6d7d411dd657da64f0
Robin <robin.braumann@gmail.com>
2020-11-05 16:16:08 +0100

    Add quiz.js, question.js, index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import {askQuestion, answerQuestion} from './quiz.js'
+
+const question = askQuestion()
+
+console.log(question)
+
+const answer = answerQuestion(question, 'b')
+
+console.log(answer ? 'correct' : 'incorrect')
---
+++ quiz.js
@@ -0,0 +1,25 @@
+import {questions} from './question.js'
+
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/math.random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+export function askQuestion(){
+    let question = questions[getRandomInt(questions.length)]
+    delete question.correctAnswer
+    return question
+}
+
+export function answerQuestion(user_question, answer){
+    questions.forEach(question => {
+        if (question.question === user_question){
+            if(answer === question.correctAnswer){
+                return true
+            }
+            else {
+                return false
+            }
+        } 
+    })
+}
