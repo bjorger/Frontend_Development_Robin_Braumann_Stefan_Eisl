52843167154e493c2086bcd146ee567690b925ac
stefan <seisl.mmt-m2020@fh-salzburg.ac.at>
2020-11-05 18:08:35 +0100

    fixed the anserQuestion 'method' by replacing the async forEach loop with for of

--- quiz.js
+++ quiz.js
@@ -1,33 +1,26 @@
-import { questions } from './question.js';
+import {questions} from './question.js'
 
 // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/math.random
 function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+export function askQuestion(){
+    const question = questions[getRandomInt(questions.length)]
+    return {
+        question: question.question,
+        a: question.a,
+        b: question.b,
+        c: question.c,
+        d: question.d
+    }
 }
 
-export function askQuestion() {
-    // Make deep copy of questions, so you can manipulate the properties
-    const questions_copy = JSON.parse(JSON.stringify(questions))
-    var question = questions_copy[getRandomInt(questions.length)];
-    delete question.correctAnswer
-	return question;
-}
-
-export function answerQuestion(user_question, answer) {
-	if (answer === questions[user_question.id].correctAnswer) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/** Solution without a Question ID
- export function answerQuestion(user_question, answer){
-    questions.forEach(question => {
-        if (question.question === user_question){
+export function answerQuestion(user_question, answer){
+    console.log(`${user_question.question} ${answer}`)
+    for(const question of questions) {
+        if (question.question === user_question.question){
             return answer === question.correctAnswer;
-        } 
-    })
+        }
+    }
 }
- * 
- */
