71bbbff8c4eca527c0b3ce2b291bd4adb78e091a
Stefan Eisl <seisl.mmt-m2020@fh-salzburg.ac.at>
2020-11-23 11:35:42 +0100

    reimplemented the console.log() statement which displays the question

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import {askQuestion, answerQuestion} from './quiz.js'
 
 const question = askQuestion()
+    .then(question => {
+        console.log(question);
+        return question;})
     .then(question => answerQuestion(question, 'b')
         .then(answer => console.log(answer ? 'correct' : 'incorrect'))
     );
